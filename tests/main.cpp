a2c292e1c9ae039ba5483e677bdfe36e33df25a9
sgriffin53 <sgriffin53@gmail.com>
2020-12-18 20:36:05 +0000

    Formatted files with clang-format.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-#include <iomanip>
 #include "perft.hpp"
+#include <iomanip>
+#include <iostream>
 
 int main() {
-    std::cout << std::boolalpha;   
+	std::cout << std::boolalpha;
 
-    std::cout << "Raven tests\n\n";
-    std::cout << "Perft: " << test::perft() << "\n";
-    return 0;
+	std::cout << "Raven tests\n\n";
+	std::cout << "Perft: " << test::perft() << "\n";
+	return 0;
 }
--- tests/perft.hpp
+++ tests/perft.hpp
@@ -10,26 +10,26 @@
 namespace test {
 
 bool perft() {
-    std::pair<std::string, std::vector<int>> tests[] = {
-        {{"startpos"}, {1, 20, 400, 8902, 197281}},
-        {{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"}, {1, 48, 2039, 97862}},
-    };
+	std::pair<std::string, std::vector<int>> tests[] = {
+		{{"startpos"}, {1, 20, 400, 8902, 197281}},
+		{{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"}, {1, 48, 2039, 97862}},
+	};
 
-    Position pos;
+	Position pos;
 
-    for (auto &[fen, nodes] : tests) {
-        for(auto i = 0; i < nodes.size(); ++i) {
-            parsefen(&pos, fen);
-            const auto result = perft(&pos, i);
-            if (result != nodes.at(i)) {
-                return false;
-            }
-        }
-    }
+	for (auto &[fen, nodes] : tests) {
+		for (auto i = 0; i < nodes.size(); ++i) {
+			parsefen(&pos, fen);
+			const auto result = perft(&pos, i);
+			if (result != nodes.at(i)) {
+				return false;
+			}
+		}
+	}
 
-    return true;
+	return true;
 }
 
-}
+} // namespace test
 
 #endif
